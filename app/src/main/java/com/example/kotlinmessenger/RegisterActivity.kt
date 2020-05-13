5174dd05c764d0fb070fcead06a5049e9b913d75
Admin <arab1997>
2020-05-13 17:03:30 +0500

    1

--- app/src/main/java/com/example/kotlinmessenger/RegisterActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/RegisterActivity.kt
@@ -11,6 +11,7 @@ import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -22,7 +23,7 @@ class RegisterActivity : AppCompatActivity() {
 
 
         button_register.setOnClickListener {
-          performRegister()
+            performRegister()
         }
 
 
@@ -39,11 +40,10 @@ class RegisterActivity : AppCompatActivity() {
 
             Log.d("MainActivity", "Try to show photo selector")
 
-            val intent =Intent(Intent.ACTION_PICK)
+            val intent = Intent(Intent.ACTION_PICK)
             intent.type = "image/*"
             startActivityForResult(intent, 0)
         }
-
 
 
     }
@@ -53,49 +53,59 @@ class RegisterActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == 0 &&  resultCode == Activity.RESULT_OK && data != null){
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
             //protected and check what the selected image was...
             Log.d("MainActivity", "Photo was selected")
 
             selectedPhotoUri = data.data
-            val bitmap= MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
 
-            val bitmapDrawable =BitmapDrawable(bitmap)
+            val bitmapDrawable = BitmapDrawable(bitmap)
             button_register.setBackgroundDrawable(bitmapDrawable)
         }
     }
 
 
-
     private fun performRegister() {
-        val email =  email_edittext_reg.text.toString()
-        val password =  password_edittext_reg.text.toString()
+        val email = email_edittext_reg.text.toString()
+        val password = password_edittext_reg.text.toString()
 
         if (email.isEmpty() || password.isEmpty()) {
             Toast.makeText(this, "Please enter text in email/pw", Toast.LENGTH_SHORT).show()
             return
 
         }
-        Log.d("MainActivity", "Email is: "+ email)
+        Log.d("MainActivity", "Email is: " + email)
         Log.d("MainActivity", "Password is:$password")
 
         //Firebase
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener{
-                if (!it.isSuccessful)return@addOnCompleteListener
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
 
                 //else if successful
-                Log.d("Main", "Successfully created user with uid: ${it.result?.user?.uid}") /* it.result.user.uid    shunaqa edi*/
+                Log.d(
+                    "Main",
+                    "Successfully created user with uid: ${it.result?.user?.uid}"
+                ) /* it.result.user.uid    shunaqa edi*/
 
                 uploadImageToFirebaseStorage()
             }
-            .addOnFailureListener{
+            .addOnFailureListener {
                 Log.d("Main", "Failed to create: ${it.message}")
                 Toast.makeText(this, "Failed to create: ${it.message}", Toast.LENGTH_SHORT).show()
             }
     }
 
     private fun uploadImageToFirebaseStorage() {
-
+        if (selectedPhotoUri == null) return
+        val filename = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("/images/$filename")
+        ref.putFile(selectedPhotoUri!!)
+            .addOnSuccessListener {
+                Log.d(
+                    "Register",
+                    "Successfully uploaded image: ${it.metadata?.path}")
+            }
     }
 }
